ca3ec61f9bb830fd818b5867086374ddb4f6f406
Sergio <sagilm@unal.edu.co>
2017-09-21 13:57:36 -0500

    intentos para arreglar js del landing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -19,9 +19,23 @@ Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap-theme.min.css )
 Rails.application.config.assets.precompile += %w( font.awesome.min.css )
 
+Rails.application.config.assets.precompile += %w(bootstrap-hover-dropdown.js)
+Rails.application.config.assets.precompile += %w(bootstrap.min.js)
+Rails.application.config.assets.precompile += %w(cable.js,front.js)
+Rails.application.config.assets.precompile += %w(gmaps.init.js)
+Rails.application.config.assets.precompile += %w(gmaps.js)
+Rails.application.config.assets.precompile += %w(jquery-1.11.0.min.js)
+Rails.application.config.assets.precompile += %w(jquery.cookie.js)
+Rails.application.config.assets.precompile += %w(jquery.parallax-1.1.3.js)
+Rails.application.config.assets.precompile += %w(owl.carousel.min.js)
+Rails.application.config.assets.precompile += %w(owl.carousel.js)
+Rails.application.config.assets.precompile += %w(espond.min.js)
+Rails.application.config.assets.precompile += %w(waypoints.min.js)
+
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
+
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
