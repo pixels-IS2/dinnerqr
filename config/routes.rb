9d1f0b2e4aca8cec10db4aa96ca686f5fc539110
Sergio <sagilm@unal.edu.co>
2017-09-21 14:23:51 -0500

    scaffolds dish order & table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :tables
+  resources :dishes
+  resources :orders
     devise_for :users, controllers: { sessions: "users/sessions" }
   root to: 'welcome#index'
   get 'about_us' => 'aboutus#about'
