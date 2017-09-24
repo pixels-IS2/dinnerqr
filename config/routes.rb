346f5f85bd0067d5bd5971b0f0cc7770520dd3a6
jdpp95 <jdporrasp@unal.edu.co>
2017-09-23 22:59:31 -0500

    About Us conectado al landing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :dishes
-  devise_for :chefs
+ # devise_for :chefs
   root 'welcome#index'
 
   get 'welcome/index'
 
-
+  get 'aboutus/about' => 'aboutus#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
