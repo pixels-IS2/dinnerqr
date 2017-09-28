a933e54e7b090b2de75bee1d83fbee56c57ae3a5
Pedro Berrio <pcberriop@unal.edu.co>
2017-09-28 02:19:09 -0700

    reglas admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'menu/menu'
-
   resources :specialities
   get 'menu' => 'menu#menu'
   devise_for :clients, controllers: { registrations:"clients/registrations",omniauth_callbacks: 'clients/omniauth_callbacks'}
