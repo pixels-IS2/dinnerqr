28bc79a163290aa9d1144335b1b81ba6d395e007
Pedro Berrio <pcberriop@unal.edu.co>
2017-10-09 02:54:10 +0000

    paginate

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.paginate(:page => params[:page],:per_page => 5)
+    @comments = Comment.all
   end
 
   # GET /comments/1
