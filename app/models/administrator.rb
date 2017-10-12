7e7c8f143e5b8ac2dfeece9c89c88e725f95cd2a
jdpp95 <jdporrasp@unal.edu.co>
2017-10-12 01:24:52 -0500

    Error fixed in view

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -4,14 +4,4 @@ class Administrator < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	def reset
-		@administrator = Administrator.where(:email => params[:email]).first
-		unless @administrator.nil?
-		  Notifier.send_reset_notice(@administrator).deliver_now
-		  @administrator.send_reset_password_instructions
-		  redirect_to new_administrator_session_url
-		else
-		  redirect_to new_administrator_session_url, :flash => { :error => "That email address matches no user." }
-	end
-end
 end
