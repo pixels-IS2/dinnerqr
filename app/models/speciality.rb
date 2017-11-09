15b06ad4bccaa619bff9a622e32a77aaf7e2f0f8
Felipe <dafbuenoca@unal.edu.co>
2017-11-09 16:10:54 +0000

    validaciones

--- app/models/speciality.rb
+++ app/models/speciality.rb
@@ -1,4 +1,5 @@
 class Speciality < ApplicationRecord
   has_many :dishes
   has_many :chefs
+  validates :name, presence: true, length: { in: 6..40 }
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -2,4 +2,6 @@ class Table < ApplicationRecord
     belongs_to :waiter
     has_many :orders
     dragonfly_accessor :qrCode
+    validates :number, presence: true,  length: { in: 1..2 }, numericality: { only_integer: true }
+     validates :sector, presence: true, length: { in: 6..40 }    
 end
