f3751a0e6e97dc40daf700ef8c4cc1bfc58e6e09
jdpp95 <jdporrasp@unal.edu.co>
2017-11-09 05:08:56 -0500

    Salio otro test esto merece un commit

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -4,4 +4,15 @@ class ClientTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  #def setup
+  #	@client = Client.new(name: "fulano", email: "mantilla@depapa.com", encrypted_password: "123456")
+    #@client.save
+	#  end
+
+  #debugger
+
+  #test "please be valid" do
+  #	assert @client.valid?
+  #end
 end
