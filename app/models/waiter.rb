6ebe076448a4ff3ff358cb464254725684dbe648
jdpp95 <jdporrasp@unal.edu.co>
2017-11-09 15:21:45 -0500

    Test controllers added/fixed, missing.png (deer) working

--- app/models/waiter.rb
+++ app/models/waiter.rb
@@ -5,6 +5,6 @@ class Waiter < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many  :tables
 
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "30x30#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "30x30#" }, :default_url => "/assets/images/:style/deer.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
